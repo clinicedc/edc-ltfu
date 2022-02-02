c2716f5149af334bdf1ab59dad8883b62774a6e4
erikvw <ew2789@gmail.com>
2022-02-02 12:43:29 -0600

    fix spelling of settings attr

--- edc_ltfu/utils.py
+++ edc_ltfu/utils.py
@@ -1,9 +1,11 @@
+import pdb
+
 from django.apps import apps as django_apps
 from django.conf import settings
 
 
 def get_ltfu_model_name():
-    return getattr(settings, "EDC_LFTU_MODEL_NAME", "edc_ltfu.ltfu")
+    return getattr(settings, "EDC_LTFU_MODEL_NAME", "edc_ltfu.ltfu")
 
 
 def get_ltfu_model_cls():

cc62dcbf72278f7df3a1435bd58e0b166f84a376
erikvw <ew2789@gmail.com>
2022-09-29 16:09:18 -0500

    update test models

--- edc_ltfu/tests/models.py
+++ edc_ltfu/tests/models.py
@@ -1,11 +1,13 @@
 from edc_action_item.models import ActionModelMixin
 from edc_model.models import BaseUuidModel
 from edc_offstudy.constants import END_OF_STUDY_ACTION
+from edc_sites.models import SiteModelMixin
 from edc_visit_schedule.model_mixins import OffScheduleModelMixin
 
 
 class OffSchedule(
     OffScheduleModelMixin,
+    SiteModelMixin,
     ActionModelMixin,
     BaseUuidModel,
 ):

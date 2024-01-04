c3d73e793fa753667982cc649feeec34fc13c81e
erikvw <ew2789@gmail.com>
2024-01-03 18:55:13 -0600

    update edc_site path

--- edc_ltfu/models.py
+++ edc_ltfu/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from edc_action_item.models.action_model_mixin import ActionModelMixin
 from edc_identifier.model_mixins import NonUniqueSubjectIdentifierFieldMixin
 from edc_model.models import BaseUuidModel
-from edc_sites.models import SiteModelMixin
+from edc_sites.model_mixins import SiteModelMixin
 
 from .constants import LTFU_ACTION
 from .model_mixins import LtfuModelMixin
